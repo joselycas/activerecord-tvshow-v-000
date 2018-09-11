2a610a2d4ffd3a682b02d79ee4fed6456d97c1f6
Jos Castro <joselycas@gmail.com>
2018-09-11 03:17:07 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -13,7 +13,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.least_popular_show
-    Show.lowest_rating.order("name DESC").first
+    Show.where("rating = ?", self.lowest_rating)[0]
   end
 
   def self.ratings_sum
